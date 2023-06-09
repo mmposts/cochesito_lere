138c5f9187e1644ea52f3fe9accdb445b38bda93
Maya <fox>
2023-06-09 12:26:52 +0200

    Añadir pequeños cambios

---
+++ coche.py
@@ -0,0 +1,15 @@
+class Coche:
+    pass
+
+def __init__(self, marca: str, modelo: str, matricula: str):
+    self.marca = marca
+    self.modelo = modelo
+    self.matricula = matricula
+
+def __str__(self):
+    return f"{self.marca} - {self.modelo} - [{self.matricula}]"
+
+    
+
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from parking import Parking
+from coche import Coche
+
+parking = Parking()
+coche = Coche("Peugot", "307", "7778-MXD")
+
+print ("hola cochesito Lere")
+print (f"Coche 001: Peugot 307")
